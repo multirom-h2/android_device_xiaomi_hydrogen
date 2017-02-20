5865549bd71c182238190c670e87e2880758b2c0
BitO BSessiOn <bitobsession@gmail.com>
2017-02-20 23:47:18 +0100

    multirom: Add MultiROM support
    Add MultiROM specific files
    Specific device revision for hydrogen
    - MultiROM FSTAB tablet
    - Device initialization (block devices and touch screen)
    - TWRP and MultiROM specific params
    
    Big thanks to:
    
    - TeamWin and specially to Abdessamad Derraz (@Abdess) <derraz.abdessamad@gmail.com>
    for initial TWRP support and Parth Bhatia (@TheStrix) <parthbhatia@lineageos.org>
    - Vojtěch Boček (@Tasssadar) <vbocek@gmail.com> for MultiROM
    - @nkk71 for NO-KEXEC support
    - @zhuowei for lockscreen bug fix (and Rohan Purohit (@rohanpurohit) for showing the path)
    - Deepak Sharma (@TechExhibeo) <TechExhibeo@gmail.com> for testing and support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,28 @@
+#
+# Copyright 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
+
 DEVICE_TREE := device/xiaomi/hydrogen
 
 # Bootloader
@@ -50,6 +75,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
+#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+#TARGET_RECOVERY_QCOM_RTC_FIX := true
+BOARD_SUPPRESS_SECURE_ERASE := true
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
@@ -71,3 +99,45 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # Debug flags
 TWRP_INCLUDE_LOGCAT := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
+
+# MR config. MultiROM also uses parts of TWRP config
+TARGET_RECOVERY_IS_MULTIROM := true
+MR_NO_KEXEC := true
+MR_ALLOW_NKK71_NOKEXEC_WORKAROUND := true
+MR_CONTINUOUS_FB_UPDATE := true
+MR_DPI := xhdpi
+MR_DPI_FONT := 340
+MR_USE_MROM_FSTAB := true
+MR_FSTAB := $(DEVICE_TREE)/multirom/mrom.fstab
+MR_INPUT_TYPE := type_b
+MR_INIT_DEVICES := $(DEVICE_TREE)/multirom/mr_init_devices.c
+MR_KEXEC_MEM_MIN := 0x00200000
+MR_KEXEC_DTB := true
+MR_DEVICE_HOOKS := $(DEVICE_TREE)/multirom/mr_hooks.c
+MR_DEVICE_HOOKS_VER := 1
+#MR_DEVICE_HOOKS_VER := 4
+MR_DEVICE_VARIANTS := hydrogen
+MR_USE_QCOM_OVERLAY := true
+MR_QCOM_OVERLAY_HEADER := $(DEVICE_TREE)/multirom/mr_qcom_overlay.h
+MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
+# bootmenu
+DEVICE_RESOLUTION := 1080x1920
+MR_PIXEL_FORMAT := "RGBA_8888"
+#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+MR_DEV_BLOCK_BOOTDEVICE := true
+
+#Force populating from the emmc
+MR_POPULATE_BY_NAME_PATH := "/dev/block/platform/soc.0/7824900.sdhci/by-name"
+
+# Versioning
+TW_DEVICE_VERSION := 3
+
+include $(DEVICE_TREE)/multirom/version/MR_REC_VERSION.mk
+
+ifeq ($(MR_REC_VERSION),)
+MR_REC_VERSION := $(shell date -u +%Y%m%d)-01
+endif
+
+BOARD_MKBOOTIMG_ARGS += --board mrom$(MR_REC_VERSION)
